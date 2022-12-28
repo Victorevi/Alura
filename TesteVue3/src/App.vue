f0387b4f5d8ccb033d0990aea5ca4082626d6b6d
Victor G. Barbosa <victor_eco@outlook.com>
2022-12-28 16:39:18 -0300

    Projetos

---
+++ TesteVue3/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" style="padding-left:calc(50% - 100px); padding-right:calc(50% - 100px);">
+  <OlaMundo />
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import OlaMundo from "@/components/OlaMundo.vue";
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    OlaMundo
+  }
+});
+</script>
